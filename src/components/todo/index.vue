9a9115bd972ab2b416089ec60be4478011bb1c67
alfiehe <alfiehe@tencent.com>
2020-12-28 14:14:36 +0800

    init vue, add store

---
+++ src/components/todo/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="todo">
+
+    <div class="todo-form">
+      <a-input
+        placeholder="请输入任务"
+        class="todo-input-value"
+      />
+      <a-button type="primary">添加事项</a-button>
+    </div>
+
+    <a-list bordered :dataSource="list" class="todo-list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox>{{ item.info }}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="todo-footer">
+        <!-- 未完成的任务个数 -->
+        <span>0条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button type="primary">全部</a-button>
+          <a-button>未完成</a-button>
+          <a-button>已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a>清除已完成</a>
+      </div>
+    </a-list>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  created () {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['list'])
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.todo {
+  padding: 30px;
+  width: 860px
+}
+.todo-form {
+  display: flex;
+}
+
+.todo-input-value {
+  flex: 1;
+  margin-right: 10px;
+}
+
+.todo-list {
+  margin-top: 10px;
+}
+
+.todo-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    list: []
+  },
+  mutations: {
+    // 修改列表数据
+    initList (state, list) {
+      state.list = list
+    }
+  },
+  actions: {
+    // 获取JSON数据
+    getList (context) {
+      axios.get('/static/mock/todoList.json').then(res => {
+        console.log('===getList===res===', res)
+        context.commit('initList', res.data)
+      })
+    }
+  },
+  getters: {
+  }
+})
